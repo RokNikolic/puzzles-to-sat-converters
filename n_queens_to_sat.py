d6292cef26cff01757cd2161ab38c3dd4bc81e67
Rok Nikolič <rok.nikolic@gmail.com>
2024-04-11 22:30:29 +0200

    diagonals working

--- n_queens_to_sat.py
+++ n_queens_to_sat.py
@@ -1,13 +1,21 @@
 # nQueens to SAT converter ♕
 # Rok Nikolič 2024
 
-def cnf_writer(size, sat_array):
+def cnf_writer(sat_list):
     comment_line = f"c nQueens to sat converter, RN 2024\n"
-    file_format = "CNF"
-    variables = size**2
-    clauses = len(sat_array)
+    file_format = "cnf"
+    variables = len(sat_list[0])**2
+    clauses = len(sat_list)
     problem_line = f"p {file_format} {variables} {clauses}\n"
-    preamble = f"{comment_line}c\n{problem_line}"
+    preamble = f"{comment_line}{problem_line}"
+    clauses = ""
+    for clause in sat_list:
+        clause_string = ""
+        for variable in clause:
+            clause_string += f"{variable} "
+        clause_string += "0\n"
+        clauses += clause_string
+    return f"{preamble}{clauses}"
 
 
 def at_least_one_multi(size):
@@ -50,42 +58,61 @@ def exactly_one(item_list):
     return final_list
 
 
-def convert_queens_to_sat(size):
-    sat_array1 = []
-    sat_array1.extend(at_least_one_multi(size))
-    sat_array1.extend(at_most_one_multi(size))
-    print(sat_array1)
-
+def convert_nqueens_to_sat(size):
     main_lists = []
     diagonal_lists = []
-    for i in range(size):
+    for i in range(1, size + 1):
         # Rows
-        main_lists.append([f"{i}{j}" for j in range(size)])
+        main_lists.append([f"{i}{j}" for j in range(1, size + 1)])
         # Columns
-        main_lists.append([f"{j}{i}" for j in range(size)])
-        # Falling diagonals
-        primary_num = i
-        secondary_num = 0
-        primary_sat_array = []
-        secondary_sat_array = []
-        while primary_num < size:
-            primary_sat_array.append(f"{primary_num}{secondary_num}")
-            if primary_num != secondary_num:
-                secondary_sat_array.append(f"{secondary_num}{primary_num}")
-            secondary_num += 1
-            primary_num += 1
-        diagonal_lists.append(primary_sat_array)
-        if secondary_sat_array:
-            diagonal_lists.append(secondary_sat_array)
+        main_lists.append([f"{j}{i}" for j in range(1, size + 1)])
 
-    sat_array = []
+        # Negative diagonals
+        primary = i
+        secondary = 1
+        left_diagonals = []
+        top_diagonals = []
+        while primary < size + 1:
+            left_diagonals.append(f"{primary}{secondary}")
+            top_diagonals.append(f"{secondary}{primary}")
+            primary += 1
+            secondary += 1
+        diagonal_lists.append(left_diagonals)
+        diagonal_lists.append(top_diagonals)
+
+        # Positive diagonals
+        primary = 4 - i
+        bottom_primary = 4
+        secondary = 1
+        bottom_secondary = i
+        left_diagonals = []
+        bottom_diagonals = []
+        while bottom_secondary < size + 1:
+            left_diagonals.append(f"{primary}{secondary}")
+            bottom_diagonals.append(f"{bottom_primary}{bottom_secondary}")
+            primary -= 1
+            bottom_primary -= 1
+            secondary += 1
+            bottom_secondary += 1
+        diagonal_lists.append(left_diagonals)
+        diagonal_lists.append(bottom_diagonals)
+
+    sat_list = []
     for item_list in main_lists:
-        sat_array.extend(exactly_one(item_list))
+        sat_list.extend(exactly_one(item_list))
 
     for item_list in diagonal_lists:
-        sat_array.extend(at_most_one(item_list))
+        sat_list.extend(at_most_one(item_list))
 
-    return sat_array
+    return sat_list
 
 
-print(convert_queens_to_sat(3))
+def write_to_file(cnf_file):
+    with open("nQueens_SAT.cnf", 'w') as file:
+        file.write(cnf_file)
+
+
+problem_size = 4
+sat_array = convert_nqueens_to_sat(problem_size)
+cnf = cnf_writer(sat_array)
+write_to_file(cnf)
